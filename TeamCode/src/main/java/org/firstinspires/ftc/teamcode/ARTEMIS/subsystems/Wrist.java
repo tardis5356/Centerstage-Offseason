8926d9e49c7ebb3ff24f6ca70f90f2d28ef59ebc
thesimg <graham.simons05@gmail.com>
2024-05-19 16:57:48 -0400

    fix wrist, full RobotToIntakeCommand working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ARTEMIS/subsystems/Wrist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ARTEMIS/subsystems/Wrist.java
@@ -1,29 +1,31 @@
 package org.firstinspires.ftc.teamcode.ARTEMIS.subsystems;
+
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+
 public class Wrist extends SubsystemBase {
     Servo sWP;
-    double WRIST_TILT_INTAKE = 0.29;
-    double WRIST_TILT_DEPOSIT_LOW = 0.69;
-    double WRIST_TILT_TRANSITION = 0.17;
 
-    public Wrist(HardwareMap hardwareMap){
+    public Wrist(HardwareMap hardwareMap) {
         sWP = hardwareMap.get(Servo.class, "sWGP");
 
     }
+
     @Override
-    public void periodic(){ ;
+    public void periodic() {
     }
 
-    public void wristToDep(){sWP.setPosition(WRIST_TILT_DEPOSIT_LOW);
-
+    public void wristToDep() {
+        sWP.setPosition(BotPositions.ARM_DEPOSIT_LOW);
     }
 
-    public void wristToIntake(){sWP.setPosition(WRIST_TILT_INTAKE);
-
+    public void wristToIntake() {
+        sWP.setPosition(BotPositions.WRIST_TILT_INTAKE);
     }
 
-    public void wristToTransition(){sWP.setPosition(WRIST_TILT_TRANSITION);}
+    public void wristToTransition() {
+        sWP.setPosition(BotPositions.WRIST_TILT_TRANSITION);
+    }
 
 }
