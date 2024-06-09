3ec50d740d864cba6ce1bd2e0e62bf156b628239
tardis5356 <ftctardis@gmail.com>
2024-06-09 15:51:36 -0400

    minor edits to OptimusWrist

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DEMOS/OPTIMUS/subsystems/OptimusWrist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DEMOS/OPTIMUS/subsystems/OptimusWrist.java
@@ -2,11 +2,18 @@ package org.firstinspires.ftc.teamcode.DEMOS.OPTIMUS.subsystems;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.hardware.ServoEx;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
 public class OptimusWrist extends SubsystemBase {
-    DcMotorEx mA;
+    Servo sW;
+
+    public OptimusWrist(HardwareMap hardwareMap) {
+        sW = hardwareMap.get(Servo.class, "sW");
+
+    }
 }
