2633d579e11203af0551d30b8190dbd02f77fb89
tardis5356 <ftctardis@gmail.com>
2024-06-09 15:47:25 -0400

    Added gripper positions for primus

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DEMOS/demoBotPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DEMOS/demoBotPositions.java
@@ -5,6 +5,7 @@ public class demoBotPositions {
     public static double innitialWristPosition = .7, downWristPosition = 0, upWristPosition = 1,
             closedGripper = .35, opennedGripper = .6;
     //Primus Positions
-
+    public static double leftGripperOpen = .35, leftGripperClosed = .75,
+        rightGripperOpen = .6, rightGripperClosed = .25;
 
 }
